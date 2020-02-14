4a5f2792e0fb86daf953b0a225911c0fc1f7dfd9
Countermatt <matthieu.pgo@orange.fr>
2020-02-14 14:06:11 +0100

    initialisation de l'environnement de travail python

---
+++ python_environment/venv-IGAppSDN/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/countermatt/Documents/projet_S8/IGAppSDN/python_environment/venv-IGAppSDN/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
